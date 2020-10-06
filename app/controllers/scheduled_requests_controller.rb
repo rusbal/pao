6d610ef7016c956009450e705c49d1d7dc896b73
Raymond Usbal <raymond@philippinedev.com>
2020-10-06 22:05:43 +0800

    destroy action

--- app/controllers/scheduled_requests_controller.rb
+++ app/controllers/scheduled_requests_controller.rb
@@ -2,4 +2,10 @@ class ScheduledRequestsController < ApplicationController
   def index
     @requests = Request.scheduled
   end 
+
+  def destroy
+    request = Request.find(params[:id])
+    request.update(status: Request::STATUS_CLOSE)
+    redirect_to open_requests_url, notice: "Status was closed"
+  end
 end
