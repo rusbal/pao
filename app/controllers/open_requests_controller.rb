4d6d67219e6caa8899918f4b38b65d67e759a06a
Raymond Usbal <raymond@philippinedev.com>
2020-10-06 20:24:40 +0800

    destroy

--- app/controllers/open_requests_controller.rb
+++ app/controllers/open_requests_controller.rb
@@ -2,4 +2,10 @@ class OpenRequestsController < ApplicationController
   def index
     @requests = Request.open
   end
+
+  def destroy
+    request = Request.find(params[:id])
+    request.update(status: Request::STATUS_CLOSE)
+    redirect_to open_requests_url, notice: "Status was closed"
+  end
 end
