6623799146830e56406ae74f1f471d555442d35c
Raymond Usbal <raymond@philippinedev.com>
2020-10-06 21:41:16 +0800

    request meetings route

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,11 @@ Rails.application.routes.draw do
   resources :admins, only: :index
   resources :lawyers, only: [:index, :show]
   resources :clients, only: [:index, :show]
-  resources :requests, only: :index
+  resources :requests, only: :index do
+    resource :meetings, only: [:new, :create]
+  end
   resources :open_requests, only: [:index, :destroy]
-  resources :scheduled_requests, only: :index
+  resources :scheduled_requests, only: [:index, :destroy]
   resources :close_requests, only: :index
   resources :meetings, only: :index
 end
