ed43173157a09887a9f0bbbd412d4c4730eba94e
Raymond Usbal <raymond@philippinedev.com>
2020-10-06 20:25:59 +0800

    open req destroy

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   resources :lawyers, only: [:index, :show]
   resources :clients, only: [:index, :show]
   resources :requests, only: :index
-  resources :open_requests, only: :index
+  resources :open_requests, only: [:index, :destroy]
   resources :close_requests, only: :index
   resources :meetings, only: :index
 end
